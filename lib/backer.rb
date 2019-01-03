b6f0f56a7ad9d2d5875df71da070782f9e2137c6
Patrice Drayton <draytonpatrice@marilynssecret.org>
2019-01-03 17:07:21 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -5,13 +5,11 @@ class Backer
   
   def initialize(name)
   @name = name  
-  
-  
-  
-  
-  
-  
-end
-  
-  
+  @backed_projects = []
+  end
+
+  def back_project(project)
+  backed_projects << project
+  project.backers << self
+  end
 end
